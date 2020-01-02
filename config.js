9347b4235e0e1292c7292c87c277d106da2b2567
Sakthi IBMer <49334000+Sakthi9292-ibm@users.noreply.github.com>
2020-01-02 13:36:03 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+exports.config = {
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['UItest.js'],
+  capabilities: {
+      browserName: 'chrome',
+
+      chromeOptions: {
+          args: [ "--headless", "--disable-gpu", "--window-size=800,600" ]
+      }
+  }
+};
+
