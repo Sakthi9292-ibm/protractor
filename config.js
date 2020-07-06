9c38fc7ba8d0b363f04d3cf893c8af0c11db85dc
Sakthi IBMer <49334000+Sakthi9292-ibm@users.noreply.github.com>
2020-07-06 16:52:52 +0530

    Update config.js and jar file

--- config.js
+++ config.js
@@ -1,12 +1,14 @@
 exports.config = {
 		directConnect: true,
   //seleniumAddress: 'http://localhost:4444/wd/hub',
+			seleniumServerJar:'selenium-server-standalone-2.53.0.jar',
   specs: ['UItest.js'],
   capabilities: {
       'browserName': 'firefox',
       
       'moz:firefoxOptions': {
-    	    'args': ['--headless', '--disable-gpu', '--window-size=800,600']
+    	    //'args': ['--headless', '--disable-gpu', '--window-size=800,600']
+    	  'args': ['--disable-gpu', '--window-size=800,600']
 
       }
   }
